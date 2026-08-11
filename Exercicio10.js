f10c70e0640f2e38306b28ba92384bc28b7a7a2d
Gustavo Paulino <gupazouza@gmail.com>
2026-08-10 22:43:12 -0300

    Exercicio 10

---
+++ Exercicio10.js
@@ -0,0 +1,17 @@
+ const prompt = require('prompt-sync')();
+
+function escadaInvertida(){
+let numero = Number(prompt("Digite um numero: "));
+
+for(let i = numero;i >= 1;i --){
+let limite = "";
+
+for(let a = 1;a <= i;a++ ){
+limite += a;
+}
+
+console.log(limite);
+}
+}
+
+escadaInvertida();
